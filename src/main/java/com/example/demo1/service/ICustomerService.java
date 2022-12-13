7a4b3b4964686d6c37654b2e246974ccbc835152
vuminh98 <vuminhtran27@gmail.com>
2022-12-13 21:56:51 +0700

    a

---
+++ src/main/java/com/example/demo1/service/ICustomerService.java
@@ -0,0 +1,17 @@
+package com.example.demo1.service;
+
+import com.example.demo1.model.Customer;
+
+import java.util.List;
+
+public interface ICustomerService {
+    List<Customer> findAll();
+
+    void save(Customer customer);
+
+    Customer findById(int id);
+
+    void update(int id, Customer customer);
+
+    void remove(int id);
+}
